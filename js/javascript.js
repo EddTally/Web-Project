bf81fe6836391b43d77961b4621249fec18ad12a
Edward Tallentire <P15230019@LEC-ADMIN.dmu.ac.uk>
2017-11-30 14:19:03 +0000

    Centered Nav, made css mobile first, added 1100px breakpoint

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+
+
+      new Twitch.Embed("twitch-embed", {
+        width: 854,
+        height: 480,
+        channel: "AdmiralBulldog",
+		layout: "video",
+		autoplay: "false"
+      });
+    
