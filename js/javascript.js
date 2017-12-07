81a622089dfbf43d5690337961dd849956fe5836
Edward Tallentire <P15230019@LEC-ADMIN.dmu.ac.uk>
2017-12-07 14:58:40 +0000

    Put the twitch embed in a section with an article and formatted them

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,18 @@
 
 
-      new Twitch.Embed("twitch-embed", {
+      var embed = new Twitch.Embed("twitch-embed", {
         width: 427,
         height: 240,
-        channel: "rtz",
-		layout: "video",
-		autoplay: "false"
+        channel: "AdmiralBulldog",
+        layout: "video",
+        autoplay: false
       });
+
+      embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
+        var player = embed.getPlayer();
+        player.play();
+      });
+	  
+		  
+		  
     
