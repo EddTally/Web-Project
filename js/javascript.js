8a654fc991081ff30c2cfb10dfbf607839b19794
eddtally <edd.tallentire@gmail.com>
2017-12-17 17:44:50 +0000

    changed onclick in the html to JS file for next and previous slideshow <a>'s

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,11 @@
-	
+
 	//Bulldog SlideShow
 	//https://www.w3schools.com/howto/howto_js_slideshow.asp
+
 	
-	var slideIndex = 1;
-  showSlides(slideIndex);
+var slideIndex = 1;
+showSlides(slideIndex);
+
 
   // Next/previous controls
   function plusSlides(n) {
@@ -30,13 +32,25 @@
     slides[slideIndex-1].style.display = "block"; 
     dots[slideIndex-1].className += " active";
   }
-	
-	
-	
+  	console.log("Got to here");
+
+//nextBtn.addEventListener("click", plusSlides(1));
+//prevBtn.addEventListener("click", plusSlides(-1));
+
+/*Using query selector to select by css identifier because
+I don't want to loop through the HTML Collection*/
+
+document.querySelector(".next1").addEventListener("click", function(){
+	plusSlides(1);
+});
+document.querySelector(".prev1").addEventListener("click", function(){
+	plusSlides(-1);
+});
+
 	
 	//Twitch Vod
 	//https://dev.twitch.tv/docs/embed#interactive-frames-for-live-streams-and-vods
-	
+
 	var options = {
 		width: 401,
 		height: 301,
@@ -45,6 +59,4 @@
 	};
 	
 	var player = new Twitch.Player("twitch-vod", options);
-	player.setVolume(0.5);
-	
-	
+	player.setVolume(0.5); 
